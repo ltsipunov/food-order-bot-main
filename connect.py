ca5d6e4e8805e51426af9e5969e1e908840b72c5
ltsipunov <l.tsipunov@yandes.ru>
2025-02-23 15:33:27 +0300

    wconn implemented

--- connect.py
+++ connect.py
@@ -1,5 +1,8 @@
 import sqlite3
 conn = sqlite3.connect("db/food_orders.sqlite",check_same_thread=False)
-conn.execute("PRAGMA journal_mode=WAL")
 conn.row_factory = sqlite3.Row
 
+def conn_to_write():
+    wconn = sqlite3.connect("db/food_orders.sqlite")
+    wconn.execute("PRAGMA journal_mode=WAL")
+    return  wconn
